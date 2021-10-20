a140517994cce58215d85025f3459a5d8e7b1ddc
Ivanova Anna <annprokudina@gmail.com>
2021-10-20 10:12:22 +0300

    added logic for switching colors

--- hw_1.2/ViewController.swift
+++ hw_1.2/ViewController.swift
@@ -25,9 +25,35 @@ class ViewController: UIViewController {
         yellowRectangleView.layer.cornerRadius = yellowRectangleView.frame.size.width/2
         
         greenRectangleView.layer.cornerRadius = greenRectangleView.frame.size.width/2
+        
     }
+    
+    var count = 2
 
     @IBAction func switchColorPressed() {
+        
+        let customButtonTitle = NSMutableAttributedString(string: "NEXT", attributes: [
+            NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 40)
+        ])
+        trafficLightSwitchButton.setAttributedTitle(customButtonTitle, for: .normal)
+        
+        
+        count += 1
+        switch count % 3 {
+        case 0:
+            redRectangleView.alpha = 1
+            greenRectangleView.alpha = 0.3
+        case 1:
+            yellowRectangleView.alpha = 1
+            redRectangleView.alpha = 0.3
+        case 2:
+            greenRectangleView.alpha = 1
+            yellowRectangleView.alpha = 0.3
+        default:
+            redRectangleView.alpha = 0.3
+            yellowRectangleView.alpha = 0.3
+            greenRectangleView.alpha = 0.3
+        }
     }
     
 }
