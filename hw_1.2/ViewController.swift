407c311248d116b9f772ae5d8abf0066493f8e84
Ivanova Anna <annprokudina@gmail.com>
2021-10-20 08:55:51 +0300

    rounding the view

--- hw_1.2/ViewController.swift
+++ hw_1.2/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redRectangleView: UIView!
+    
+    @IBOutlet var yellowRectangleView: UIView!
+    
+    @IBOutlet var greenRectangleView: UIView!
+    
+    @IBOutlet var trafficLightSwitchButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redRectangleView.layer.cornerRadius = redRectangleView.frame.size.width/2
+        
+        yellowRectangleView.layer.cornerRadius = yellowRectangleView.frame.size.width/2
+        
+        greenRectangleView.layer.cornerRadius = greenRectangleView.frame.size.width/2
     }
 
-
+    @IBAction func switchColorPressed() {
+    }
+    
 }
 
